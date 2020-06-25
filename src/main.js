27854c548d2594c0567f81b91f7c3ebf2599ea5d
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-06-25 12:30:47 -0500

    hola

--- src/main.js
+++ src/main.js
@@ -8,12 +8,17 @@ var boton = document.querySelector('#boton')
 boton.addEventListener('click',traer());
 
 function traer(){
+  
+
     fetch('https://rickandmortyapi.com/api/character/')
     .then(res => res.json())
     .then(data =>{
         content.innerHTML=`
-        <img src="${data.results[0].image}" alt="" width="100px">
-      <p>Nombre:</p>
+        <img src="${data.results[0].image}" alt="" width="500px">
+      <p>Nombre:${data.results[5].name}</p>
+      <p>status:${data.results[0].status}</p>
+      <p>especie:${data.results[0].species}</p>
+      <p>origen:${data.results[0].location.name}</p>
         `
     })
 }
