0fb604aa32f52478662c4254024ef0f382f90408
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-06-25 11:43:26 -0500

    nuevos cambios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+var content = document.querySelector('#contenido')
+var boton = document.querySelector('#boton')
+boton.addEventListener('click',traer());
+
+function traer(){
+    fetch('https://rickandmortyapi.com/api/character/')
+    .then(res => res.json())
+    .then(data =>{
+        content.innerHTML=`
+        <img src="${data.results[0].image}" alt="" width="100px">
+      <p>Nombre:</p>
+        `
+    })
+}
